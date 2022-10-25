4071a39820e9cbaa1484908a0af9f8fb136d7014
Jimjimka <106761382+Jimjimka@users.noreply.github.com>
2022-10-25 22:19:20 +0300

    Done all

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const throttle = require('lodash.throttle');
+const feedbackformEl =document.querySelector('.feedback-form');
 
+const feedbackFormState={}
+
+const fillfeedbackFormFields=()=>{
+    let contactFormData
+    try {
+        contactFormData= JSON.parse(localStorage.getItem('feedbackFormState'))
+        if (contactFormData===null){
+            return
+        }
+    } catch(err){
+        console.log(err);
+    } 
+    for(let el in contactFormData ){
+        if(contactFormData.hasOwnProperty(el)){
+            feedbackformEl.elements[el].value=contactFormData[el]
+
+        }
+        
+    }
+
+}
+fillfeedbackFormFields()
+
+const onFormFiledChange=(event)=>{
+    const {target}=event
+    const fieldValue=target.value;
+    const fieldName=target.name;
+    feedbackFormState[fieldName]=fieldValue
+    localStorage.setItem('feedbackFormState',JSON.stringify(feedbackFormState))
+}
+
+const onContactsFormSubmit = event=>{
+    event.preventDefault()
+    localStorage.removeItem('feedbackFormState')
+    feedbackformEl.reset()
+
+
+}
+
+feedbackformEl.addEventListener('input',throttle(onFormFiledChange, 500 ))
+feedbackformEl.addEventListener('submit',onContactsFormSubmit)
